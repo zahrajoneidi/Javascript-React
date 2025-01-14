bf7b0cdd34d6cafd95d9523c67151f2c6a68c417
zahrajoneydi <zahra.joneydi@hyperisland.se>
2025-01-14 10:11:04 +0100

    pull assignment6

---
+++ 6/script.js
@@ -0,0 +1,23 @@
+// TODO: Set item in localStorage
+
+// TODO: Get item from localStorage
+
+// TODO: Remove item from localStorage
+
+// TODO: Clear all items from localStorage
+
+// TODO: Add an object to localStorage
+
+// TODO: Get an object from localStorage
+
+// TODO: Set item in sessionStorage
+
+// TODO: Get item from sessionStorage
+
+// TODO: Remove item from sessionStorage
+
+// TODO: Clear all items from sessionStorage
+
+// TODO: Add an object to sessionStorage
+
+// TODO: Get an object from sessionStorage
